647a4fbc3ceee2d6c67d1db8ee078f5a19823ff0
maxime-becker <maximetks@gmail.com>
2022-11-11 14:13:07 +0100

    en s'en fou

--- PockerApp/PockerApp/Cartes.cs
+++ PockerApp/PockerApp/Cartes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,10 @@ namespace PockerApp
 {
     internal class Cartes
     {
-        
+        public static int compt()
+        {
+            return 0;
+        }
     }
+    
 }
